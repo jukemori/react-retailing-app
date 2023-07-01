cb0a25dab7ec3c2ba49d6a40f522ef6ced5d8611
Jun Ukemori <jukemori@gmail.com>
2023-07-01 18:20:06 +0900

    display item page

--- app/src/pages/Category.tsx
+++ app/src/pages/Category.tsx
@@ -1,10 +1,25 @@
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useItems } from "../data/api";
 import { Tabs } from "../components/Tabs";
 
 export function Category() {
+  const { id } = useParams();
+  const items = useItems();
+
+  // Filter items by category_id
+  const filteredItems = items.filter((item) => item.category_id === Number(id));
+
   return (
     <>
       <Tabs />
-      <h1>category</h1>
+      {filteredItems.map((item) => (
+        <div key={item.id}>
+          <Link to={`/item/${item.id}`}>
+            <img src={item.image} alt={item.name} />
+          </Link>
+        </div>
+      ))}
     </>
   );
 }
--- app/src/pages/Item.tsx
+++ app/src/pages/Item.tsx
@@ -1,3 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useItemById } from "../data/api";
+
 export function Item() {
-  return <h1>Item</h1>;
+  const { id } = useParams();
+  const item: any = useItemById(Number(id));
+
+  if (!item) {
+    return <div>Loading...</div>; // Handle the case when the item is not found or still loading
+  }
+
+  return (
+    <>
+      <div>
+        <img src={item.image} alt={item.name} />
+        <h2>{item.name}</h2>
+        <p>{item.comment_count} comments</p>
+        <p>{item.like_count} likes</p>
+        <p>{item.price} yen</p>
+        <p>shipping fee: {item.shipping_fee}</p>
+        <p>{item.description}</p>
+        {/* Render other details of the item */}
+      </div>
+    </>
+  );
 }
