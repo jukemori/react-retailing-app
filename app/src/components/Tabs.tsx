682103961e38dfc0908ca2096896cb754e47ad9f
Jun Ukemori <jukemori@gmail.com>
2023-07-01 14:56:20 +0900

    routes for lists

---
+++ app/src/components/Tabs.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+export function Tabs() {
+  return (
+    <div className="tabs">
+      <div className="tab-container">
+        <NavLink to="/" className="tab">
+          Home
+        </NavLink>
+        <NavLink to="/store" className="tab">
+          Store
+        </NavLink>
+        <NavLink to="/about" className="tab">
+          About
+        </NavLink>
+      </div>
+    </div>
+  );
+}
