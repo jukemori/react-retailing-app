9e9909e769a57de2e84f7977c7fa343c94f02134
Jun Ukemori <jukemori@gmail.com>
2023-07-02 11:12:31 +0900

    active tabs set

--- app/src/components/Tabs.tsx
+++ app/src/components/Tabs.tsx
@@ -1,25 +1,33 @@
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useCategories } from "../data/GetCategories";
 import "./Tabs.css";
 
 export function Tabs() {
   const categories = useCategories();
+  //assigning location variable
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split("/");
   return (
-    <div className="tabs">
-      <div className="tab-container">
-        <NavLink to="/" className="tab">
-          すべて
-        </NavLink>
+    <>
+      <ul>
+        {/* Checking the current path name using javascript ternary operator and if true adding active classname to it */}
+        <li className={splitLocation[1] === "" ? "active" : ""}>
+          <NavLink to="/">すべて</NavLink>
+        </li>
         {categories.map((category) => (
-          <NavLink
-            to={`/category/${category.id}`}
-            className="tab"
-            key={category.id}
-          >
-            {category.name}
-          </NavLink>
+          <li className={splitLocation[3] === category.id ? "active" : ""}>
+            <NavLink to={`/category/${category.id}`} key={category.id}>
+              {category.name}
+            </NavLink>
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </>
   );
 }
