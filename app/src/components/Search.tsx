1f20439d9268f6c3601f36dfc1ab1bf0669ef068
Jun Ukemori <jukemori@gmail.com>
2023-07-04 13:45:23 +0900

    search bar added

--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useItems } from "../data/GetItems";
+import { MagnifyingGlass } from "./Icons";
+import "./Search.css";
 
 export function Search() {
   const navigate = useNavigate();
   const items = useItems();
   const [value, setValue] = useState("");
+  const [searchPerformed, setSearchPerformed] = useState(false); // New state variable
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+    setSearchPerformed(false); // Reset searchPerformed when input value changes
   };
 
   const onSearch = (searchTerm: string) => {
     setValue(searchTerm);
+    setSearchPerformed(true); // Set searchPerformed to true when a search is made
     navigate(`/search?keyword=${searchTerm}`);
   };
 
@@ -24,29 +29,44 @@ export function Search() {
 
   return (
     <>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        onKeyPress={handleKeyPress}
-      />
-      <button onClick={() => onSearch(value)}>Search</button>
-      <div>
-        {items
-          .filter((item) => {
-            const searchTerm = value.toLowerCase();
-            const name = item.name.toLowerCase();
+      <div className="search-container">
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          onKeyPress={handleKeyPress}
+          className="search-input"
+          placeholder="検索"
+        />
+        <div onClick={() => onSearch(value)} className="search-button">
+          <MagnifyingGlass />
+        </div>
+        {value &&
+          !searchPerformed && ( // Conditionally render search suggestions if there is a value
+            <div className="search-suggestions">
+              {items
+                .filter((item) => {
+                  const searchTerm = value.toLowerCase();
+                  const name = item.name.toLowerCase();
 
-            return (
-              searchTerm && name.startsWith(searchTerm) && name !== searchTerm
-            );
-          })
-          .slice(0, 10)
-          .map((item) => (
-            <div key={item.id} onClick={() => onSearch(item.name)}>
-              {item.name}
+                  return (
+                    searchTerm &&
+                    name.startsWith(searchTerm) &&
+                    name !== searchTerm
+                  );
+                })
+                .slice(0, 10)
+                .map((item) => (
+                  <div
+                    key={item.id}
+                    onClick={() => onSearch(item.name)}
+                    className="search-item"
+                  >
+                    {item.name}
+                  </div>
+                ))}
             </div>
-          ))}
+          )}
       </div>
     </>
   );
